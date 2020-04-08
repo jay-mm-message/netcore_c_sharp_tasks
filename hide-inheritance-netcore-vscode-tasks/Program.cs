9d341a07d86d6932edd2962d496bd097de529151
Jay <jay.mm.message@gmail.com>
2020-04-08 09:20:29 +0800

    New example that Class of base and class of subclass can use the method with the same name.

---
+++ hide-inheritance-netcore-vscode-tasks/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace hide_inheritance_netcore_vscode_tasks
+{
+    class Base {
+        protected string Information {
+            get {
+                return "Base.";
+            }
+        }
+        public void show() {
+            Console.WriteLine($"Base information is {this.Information}");
+        }
+    }
+    class SubClass : Base {
+        new public void show() {
+            Console.WriteLine($"Subclass information is none");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Base b1 = new Base();
+            b1.show();
+            SubClass sc1 = new SubClass();
+            sc1.show();
+        }
+    }
+}
