6157c7f10dc94e24889c74be0f8c87a0cfa4879b
Jay <jay.mm.message@gmail.com>
2020-04-09 01:56:17 +0800

    New example that delegate-netcore-vscode-tasks.

---
+++ delegate-netcore-vscode-tasks/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace delegate_netcore_vscode_tasks
+{
+    delegate int NumberChanger(int n);
+    class Program
+    {
+        static int num = 10;
+        public static int AddNum(int p)
+        {
+            num += p;
+            return num;
+        }
+        public static int MultNum(int q)
+        {
+            num *= q;
+            return num;
+        }
+        public static int getNum()
+        {
+            return num;
+        }
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            NumberChanger nc1 = new NumberChanger(AddNum);
+            NumberChanger nc2 = new NumberChanger(MultNum);
+
+            nc1(25);
+            Console.WriteLine($"Value of Num: {getNum()}");
+            nc2(5);
+            Console.WriteLine($"Value of Num: {getNum()}");
+            
+        }
+    }
+}
