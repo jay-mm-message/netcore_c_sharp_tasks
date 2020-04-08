b19aa27e6bc7796a21401efcb106061fc69c64b5
Jay <jay.mm.message@gmail.com>
2020-04-08 14:38:15 +0800

    New abstract behavior.

---
+++ abstract-inheritance-netcore-vscode-tasks/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace abstract_inheritance_netcore_vscode_tasks
+{
+    abstract class Base {
+        public abstract void show();
+    }
+
+    class SubClass : Base {
+        public override void show() {
+            Console.WriteLine("inplement show of SubClass");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            SubClass sb = new SubClass();
+            sb.show();
+        }
+    }
+}
