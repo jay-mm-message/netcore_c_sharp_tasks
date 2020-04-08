5a0a6c543b9087a78d43a59607e1bbf5e92d0b7f
Jay <jay.mm.message@gmail.com>
2020-04-08 21:44:28 +0800

    New example that used constraint of generic.

---
+++ constraint_generic-netcore-vscode-tasks/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace constraint_generic_netcore_vscode_tasks
+{
+    class Program
+    {
+        private static T checkData<T>(T one, T two, T three) where T : IComparable {
+            T max =  one;
+            if (two.CompareTo(max) > 0) {
+                max = two;
+            }
+            if (three.CompareTo(max) > 0) {
+                max = three;
+            }
+
+            return max;
+        }
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            int a = 5;
+            int b = 6;
+            int c = 1;
+
+            int max = checkData(a, b, c);
+            Console.WriteLine($"a {a}, b {b}, c {c}, max is {max}");
+        }
+    }
+}
