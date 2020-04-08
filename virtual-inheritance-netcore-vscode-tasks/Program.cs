48a1899077cb5988d6a6da2ff82575c0e47ecc84
Jay <jay.mm.message@gmail.com>
2020-04-08 14:17:57 +0800

    New add virtual of BaseClass and override of SubClass.

---
+++ virtual-inheritance-netcore-vscode-tasks/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace virtual_inheritance_netcore_vscode_tasks
+{
+    class Base {
+        public virtual void show() {
+            Console.WriteLine("Base");
+        }
+    }
+    class SubClass : Base {
+        public override void show(){
+            Console.WriteLine("SubClass");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Base b1 = new Base();
+            b1.show();
+            SubClass sc = new SubClass();
+            sc.show();
+        }
+    }
+}
