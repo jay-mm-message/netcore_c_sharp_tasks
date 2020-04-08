08a0c16e0e3a93bb4330da2427c5d91637dcf4ec
Jay <jay.mm.message@gmail.com>
2020-04-08 16:27:07 +0800

    New example that interface behavior.

---
+++ interface-inheritance-netcore-vscode-tasks/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace interface_inheritance_netcore_vscode_tasks
+{
+    class SubClass : ISchool {
+        private int subject;
+        // implement ISchool
+        public int Subject { 
+            get {
+                return subject;
+            }
+            set {
+                this.subject = value;
+            } 
+        }
+        public void ShowMessage() {
+            Console.WriteLine($"subject {this.Subject}");
+        }
+
+    }
+    // define interface
+    interface ISchool {
+        // define attribute
+        int Subject { get; set; }
+        void ShowMessage();
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            SubClass subject = new SubClass();
+            subject.Subject = 200;
+            subject.ShowMessage();
+        }
+    }
+}
