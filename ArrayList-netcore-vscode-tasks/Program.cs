42cd15c581ce17e6cb9c9f0e7fc424dd7c9d426e
Jay <jay.mm.message@gmail.com>
2020-04-08 22:53:16 +0800

    New example that used ArrayList for System.Collections.

---
+++ ArrayList-netcore-vscode-tasks/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+
+namespace ArrayList_netcore_vscode_tasks
+{
+    class Program
+    {
+        private static readonly string [] Subjects = 
+        { "程式語言", "資訊數學", "計算機概論", "多媒體", "網路概論" };
+        private static readonly string [] ChoiceSubject = 
+        { "英文會話", "資訊數學", "網路概論" };
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            ArrayList list = new ArrayList(1);
+            foreach (var item in Subjects) {
+                list.Add(item);
+            }
+
+            ArrayList list2 = new ArrayList(1);
+            foreach (var item in ChoiceSubject) {
+                list2.Add(item);
+            }
+
+            foreach(var item in list){
+                Console.Write(item);
+            }
+            Console.WriteLine();
+
+            foreach(var item in list2) {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+        }
+    }
+}
